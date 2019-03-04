889274f8adeff53c1abe9b698ca41866c54459e6
palmonoczki <monoczki@hotmail.com>
2019-03-04 11:42:29 +0100

    initial commit

---
+++ Programming_Dotnet_70-483/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programming_Dotnet_70483
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
