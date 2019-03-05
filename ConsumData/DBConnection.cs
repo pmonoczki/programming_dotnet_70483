364796cedd4b6dbd166d54040c507c43411581b5
palmonoczki <monoczki@hotmail.com>
2019-03-05 19:28:01 +0100

    separated class

---
+++ ConsumData/DBConnection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Npgsql;
+
+namespace ConsumData
+{
+    public class DBConnection
+    {
+        public DBConnection()
+        {
+        }
+
+        /// <summary>
+        /// Shows the products.
+        /// </summary>
+        public void ShowProducts()
+        {
+            using (var cmd = new NpgsqlCommand("SELECT * FROM products", this.connectToDB()))
+            using (var reader = cmd.ExecuteReader())
+                while (reader.Read())
+                    Console.WriteLine(reader.GetString(0));
+        }
+    
+
+        #region Private
+
+        /// <summary>
+        /// The connection string.
+        /// </summary>
+        private static readonly String CONNECTION_STRING = "Host=127.0.0.1;Port=5432;Username=palmonoczki;Password=;Database=palmonoczki";
+
+        /// <summary>
+        /// Connects to db.
+        /// </summary>
+        /// <returns>The to db.</returns>
+        private NpgsqlConnection connectToDB()
+        {
+
+            var conn = new NpgsqlConnection(DBConnection.CONNECTION_STRING);
+            conn.Open();
+
+            return conn;
+
+        }
+        
+
+        #endregion
+
+
+    }
+
+
+}
--- ConsumData/Program.cs
+++ ConsumData/Program.cs
@@ -7,27 +7,11 @@ namespace ConsumData
     {
         static void Main(string[] args)
         {
-            Program p = new Program();
-            p.connectToDB();
+            DBConnection db = new DBConnection();
+            db.ShowProducts();
         }
 
-        private void connectToDB()
-        {
-            var connString = "Host=127.0.0.1;Port=5432;Username=palmonoczki;Password=;Database=palmonoczki";
 
-            using (var conn = new NpgsqlConnection(connString))
-            {
-                conn.Open();
 
-             
-
-                using (var cmd = new NpgsqlCommand("SELECT * FROM products", conn))
-                using (var reader = cmd.ExecuteReader())
-                    while (reader.Read())
-                        Console.WriteLine(reader.GetString(0));
-            }
-
-        }
-    
     }
 }
