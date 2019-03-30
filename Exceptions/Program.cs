f0a4b099c0f9506d8988fb0188a11c45c977ed7c
palmonoczki <monoczki@hotmail.com>
2019-03-30 15:37:30 +0100

    exception handling project has been added

---
+++ Exceptions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            strng s = Console.ReadLine();
+            int a = int.Parse(s);
+            if (a == 42)
+                Environment.FailFast("hoppá");
+        }    
+    }
+}
